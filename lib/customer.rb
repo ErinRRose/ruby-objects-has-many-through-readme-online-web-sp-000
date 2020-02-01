397d00663e4a51030d6ef97803b72d6f14276a83
Erin  Rose <errose10@gmail.com>
2020-02-01 23:55:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
-  
+  attr_accessor :name, :age
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def sef.all
+    @@all
+  end
+
 end
